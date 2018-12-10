14ee5ed2367f41bcd6d19666af7a9bd6773425cf
MorokoJean <shwteams@gmail.com>
2018-12-10 11:50:27 +0000

    element de Admin

---
+++ AdminLTE-master/plugins/Chart.js-master/src/chart.js
@@ -0,0 +1,227 @@
+/**
+ * @namespace Chart
+ */
+//var Chart = require('./core/core.js')();
+
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.helpers')(Chart);
+
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.helpers.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./platforms/platform.js')(Chart);
+
+require(['AdminLTE-master/plugins/Chart.js-master/src/platforms/platform.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.canvasHelpers')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.canvasHelpers.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.element')(Chart);
+
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.element.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.plugin.js')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.plugin.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./core/core.animation')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.animation.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./core/core.controller')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.controller.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./core/core.datasetController')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.datasetController.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.layoutService')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.layoutService.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./core/core.scaleService')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.scaleService.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.ticks.js')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.ticks.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./core/core.scale')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.scale.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.interaction')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.interaction.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./core/core.tooltip')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/core/core.tooltip.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./elements/element.arc')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/elements/element.arc.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./elements/element.line')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/elements/element.line.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./elements/element.point')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/elements/element.point.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./elements/element.rectangle')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/elements/element.rectangle.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./scales/scale.linearbase.js')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.linearbase.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./scales/scale.category')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.category.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./scales/scale.linear')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.linear.js'], function (foo) {
+    //foo is now loaded.
+});
+
+
+//require('./scales/scale.logarithmic')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.logarithmic.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./scales/scale.radialLinear')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.radialLinear.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./scales/scale.time')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/scales/scale.time.js'], function (foo) {
+    //foo is now loaded.
+});
+// Controllers must be loaded after elements
+// See Chart.core.datasetController.dataElementType
+//require('./controllers/controller.bar')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.bar.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./controllers/controller.bubble')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.bubble.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./controllers/controller.doughnut')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.doughnut.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./controllers/controller.line')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.line.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./controllers/controller.polarArea')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.polarArea.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./controllers/controller.radar')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/controllers/controller.radar.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Bar')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Bar.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Bubble')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Bubble.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Doughnut')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Doughnut.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Line')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Line.js'], function (foo) {
+    //foo is now loaded.
+});
+//require('./charts/Chart.PolarArea')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.PolarArea.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Radar')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Radar.js'], function (foo) {
+    //foo is now loaded.
+});
+
+//require('./charts/Chart.Scatter')(Chart);
+require(['AdminLTE-master/plugins/Chart.js-master/src/charts/Chart.Scatter.js'], function (foo) {
+    //foo is now loaded.
+});
+// Loading built-it plugins
+var plugins = [];
+
+plugins.push(
+//    require('./plugins/plugin.filler.js')(Chart),
+    require(['AdminLTE-master/plugins/Chart.js-master/src/plugins/plugin.filler.js'], function (foo) {
+    //foo is now loaded.
+    }),
+//    require('./plugins/plugin.legend.js')(Chart),
+    require(['AdminLTE-master/plugins/Chart.js-master/src/plugins/plugin.legend.js'], function (foo) {
+    //foo is now loaded.
+    }),
+    
+//    require('./plugins/plugin.title.js')(Chart)
+    require(['AdminLTE-master/plugins/Chart.js-master/src/plugins/plugin.title.js'], function (foo) {
+    //foo is now loaded.
+    })
+);
+
+Chart.plugins.register(plugins);
+
+module.exports = Chart;
+if (typeof window !== 'undefined') {
+	window.Chart = Chart;
+}
